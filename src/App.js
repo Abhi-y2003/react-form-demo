1ae9b6deed4025500c15bf884f703895e3cbb2e5
Abhi-y2003 <22633001@gkv.ac.ingit config --global user.name Abhi-y2003>
2024-03-20 22:50:41 +0530

    Project Done

--- src/App.js
+++ src/App.js
@@ -38,32 +38,33 @@ function App() {
   return (
 
 
-    <div className="border-red-800 border f">
-      <form className='flex flex-col' onSubmit={submitHandler}>
+    <div className="">
+      <div className="mx-auto my-3 w-full md:max-w-[50%] p-8 shadow-sm  rounded-lg">
+      <form className='flex flex-col gap-2' onSubmit={submitHandler}>
         
       <label htmlFor='firstName'>First Name</label>   
       <input type='text' placeholder='Abhishek' id='firstName'
-      name="firstName" onChange={changeHandler}>     
+      name="firstName" onChange={changeHandler} className="border-blue-600 border rounded-lg h-9 px-3">     
       </input>
       <br></br>
 
       
       <label htmlFor='lastName'>Last Name</label>
       <input type='text' placeholder='singh' id='lastName'
-      name="lastName" onChange={changeHandler}>
+      name="lastName" onChange={changeHandler} className="border-blue-600 border rounded-lg h-9 px-3">
       </input>
       <br></br>
 
 
       <label htmlFor='email'>Email</label>
       <input type='email' placeholder='abhishek@gmail.com' id='email'
-      name="email" onChange={changeHandler}> 
+      name="email" onChange={changeHandler} className="border-blue-600 border rounded-lg h-9 px-3"> 
       </input>
       <br></br>
 
 
       <label htmlFor='country'>Country</label>
-      <select id='country' name="country" onChange={changeHandler}>
+      <select id='country' name="country" onChange={changeHandler} className="border-blue-600 border rrounded-lg h-9 px-3">
         <option>India</option>
         <option>Usa</option>
         <option>Mexico</option>
@@ -73,89 +74,92 @@ function App() {
 
       <label htmlFor='street'>Street</label>
       <input type='text' placeholder='123 street' id='street'
-      name="street" onChange={changeHandler}>
+      name="street" onChange={changeHandler} className="border-blue-600 border rounded-lg h-9 px-3">
       </input>
       <br></br>
 
       <label htmlFor='city'>City</label>
       <input type='text' placeholder='Delhi' id='city'
-      name="city" onChange={changeHandler}>
+      name="city" onChange={changeHandler } className="border-blue-600 border rounded-lg h-9 px-3">
       </input>
       <br></br>
 
       <label htmlFor='state'>State</label>
       <input type='text' placeholder='Uttar Pradesh' id='state'
-      name="state" onChange={changeHandler}>
+      name="state" onChange={changeHandler} className="border-blue-600 border rounded-lg h-9 px-3">
       </input>
       <br></br>
 
       <label htmlFor='postalCode'>ZIP / Postal Code</label>
       <input type='text' placeholder='202001' id='postalCode'
-      name="postalCode" onChange={changeHandler}>
+      name="postalCode" onChange={changeHandler} className="border-blue-600 border rounded-lg h-9 px-3">
       </input>
       <br></br>
 
-      <p>By Emial</p>
+      <p>By Email</p>
       <div className='flex'>
-        <div className='flex'>
+        <div className='flex h-6 item-center'>
       <input type='checkbox' id='comments' name="isComments" onChange={changeHandler}></input>
       </div>
-      <div>
-      <label htmlFor='comments'>Comments</label>
-      <p>Get notified when someones posts a comment on a posting.</p>
+      <div className="ml-3 text-sm leading-6">
+      <label htmlFor='comments' className="font-medium text-gray-900">Comments</label>
+      <p className="text-gray-500">Get notified when someones posts a comment on a posting.</p>
       </div>
       </div>
 
       <div className='flex'>
-        <div className='flex'>
+        <div className='flex h-6 items-center'>
       <input type='checkbox' id='candidates' name="isCandidates" onChange={changeHandler}></input>
       </div>
-      <div>
-      <label htmlFor='candidates'>Candidates</label>
-      <p>Get notified when a candidate applies for a job.</p>
+      <div className="ml-3 text-sm leading-6">
+      <label htmlFor='candidates' className="font-medium text-gray-900">Candidates</label>
+      <p className="text-gray-500">Get notified when a candidate applies for a job.</p>
       </div>
       </div>
 
       <div className='flex'>
-        <div className='flex'>
+        <div className='flex h-6 items-center'>
       <input type='checkbox' id='offers' name="isOffers" onChange={changeHandler}></input>
       </div>
-      <div>
-      <label htmlFor='offers'>Offers</label>
-      <p>Get notified when a candidate accepts or rejects an offer.</p>
+      <div className="ml-3 leading-6 text-sm">
+      <label htmlFor='offers' className="font-medium text-gray-900">Offers</label>
+      <p className="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
       </div>
       </div>
 
 
       <div>
-        <h3>
+        <div className="text-sm leading-6">
+        <h3 className="font-medium text-lg text-gray-900">
           Push Notifications
         </h3>
-        <p>
+        <p className="text-gray-500">
           These are delivered via SMS to your mobile phone.
         </p>
+        </div>
 
-        <div>
+        <div className="my-3">
         <input type="radio" id="everything" name="options"
         value="Everything" onChange={changeHandler} checked={formData.options === "Everything"}></input>
-        <label htmlFor="everything">Everything</label>
+        <label htmlFor="everything" className="ml-2 text-sm text-gray-900 font-medium">Everything</label>
         </div>
 
-        <div>
+        <div className="my-3">
         <input type="radio" id="same" name="options"
         value="sameAsEverything" onChange={changeHandler} checked={formData.options === "sameAsEverything"}></input>
-        <label htmlFor="same">Same as Everything</label>
+        <label htmlFor="same" className="ml-2 text-sm text-gray-900 font-medium">Same as Everything</label>
         </div>
 
-        <div>
+        <div className="my-3">
         <input type="radio" id="nopushnotification" name="options" onChange={changeHandler}
         value='noPushNotifications' checked={formData.options === "noPushNotifications"} ></input>
-        <label htmlFor="nopushnotification">No push Notifications</label>
+        <label htmlFor="nopushnotification" className="ml-2 text-sm text-gray-900 font-medium">No push Notifications</label>
         </div>
       </div>
 
-      <button>Save</button>
+      <button className="bg-blue-500 text-white h-8 w-16 font-medium text-lg rounded-lg">Save</button>
       </form>
+      </div>
     </div>
   );
 }
